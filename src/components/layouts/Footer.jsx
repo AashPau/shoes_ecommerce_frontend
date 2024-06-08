5bd705b8b24b5947f3f0eeb501c44371508603b0
Aash <31497659+AashPau@users.noreply.github.com>
2024-06-09 09:39:27 +1000

    add more content to footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,6 +1,40 @@
+import { Container } from "react-bootstrap";
+import "./Footer.css";
+
 export const Footer = () => {
-  return <div className="text-center bg-black text-white-50 p-3">
-    <div></div>
-    <div> &copy; made with fun by Aashish</div>
-   </div>;
+  return (
+    <div className="bg-black py-4">
+      <Container>
+        <div className="text-center text-white-50 g-2 p-3">
+          <div className="d-flex align-items-center gap-2 justify-content-around">
+            <div className="border footerBoxesBg p-3">Contact Us</div>
+            <div className="border footerBoxesBg p-3">Order Status</div>
+            <div className="border footerBoxesBg p-3">Shipping & Delivery</div>
+            <div className="border footerBoxesBg p-3">Store Finder</div>
+            <div className="border footerBoxesBg p-3">Returns & Refunds</div>
+            <div className="border footerBoxesBg p-3">Help Center</div>
+          </div>
+          <div>
+            <div>
+              <div> Legal Information</div>
+
+              {/* Accordian UL/LI */}
+            </div>
+            <div>
+              <div>About</div>
+              {/* Accordian  */}
+            </div>
+          </div>
+          <div>
+            <hr /> <div> &copy; made with fun by Aashish</div>
+            <hr />
+          </div>
+          <div>
+            Prices subject to change without notice. Products shown may not be
+            available in our stores.
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 };
