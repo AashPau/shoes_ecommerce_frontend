275244ff451690b9b0a6427de4cfdb47397f0741
Aash <31497659+AashPau@users.noreply.github.com>
2024-06-09 10:15:26 +1000

    arrange the links in footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,11 +1,11 @@
-import { Container } from "react-bootstrap";
+import { Accordion, Container } from "react-bootstrap";
 import "./Footer.css";
 
 export const Footer = () => {
   return (
     <div className="bg-black py-4">
       <Container>
-        <div className="text-center text-white-50 g-2 p-3">
+        <div className="text-center text-white g-2 p-3">
           <div className="d-flex align-items-center gap-2 justify-content-around">
             <div className="border footerBoxesBg p-3">Contact Us</div>
             <div className="border footerBoxesBg p-3">Order Status</div>
@@ -14,15 +14,26 @@ export const Footer = () => {
             <div className="border footerBoxesBg p-3">Returns & Refunds</div>
             <div className="border footerBoxesBg p-3">Help Center</div>
           </div>
-          <div>
+          <div className="d-flex align-items-start flex-column text-start">
             <div>
-              <div> Legal Information</div>
-
-              {/* Accordian UL/LI */}
-            </div>
-            <div>
-              <div>About</div>
-              {/* Accordian  */}
+              <div>
+                <div> Legal Information</div>
+                <ul>
+                  <li>Terms $ Conditions</li>
+                  <li>Promotion Terms & Conditions </li>
+                  <li>Privacy Policy </li>
+                  <li>Cookie Seetings</li>
+                </ul>
+              </div>
+              <div>
+                <div>About</div>
+                <ul>
+                  <li>Store Locator</li>
+                  <li>All Stores</li>
+                  <li>About Foot Locker</li>
+                  <li>Careers</li>
+                </ul>
+              </div>
             </div>
           </div>
           <div>
